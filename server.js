f56c0cac302599b8cd3c0b441f2ddb12b31c392e
Jeff Crawford <jeffrey.a.crawford@gmail.com>
2018-04-24 19:10:50 -0400

    populated other pages

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ var path = require("path")
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
-})
+});
+
+app.get('/tables', function (req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get('/reserve', function (req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
  
 
 
