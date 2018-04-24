3f88f28a8c85caa5948d597038750e3e2e89e684
Jeff Crawford <jeffrey.a.crawford@gmail.com>
2018-04-24 19:08:28 -0400

    phase 2

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var PORT = 3000;
 var path = require("path") 
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "home.html"));
 })
  
 
