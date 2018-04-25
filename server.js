a0acc687842832a092f90b35cdf346e6fcf2c0e2
Jeff Crawford <jeffrey.a.crawford@gmail.com>
2018-04-24 20:02:40 -0400

    push to array

--- server.js
+++ server.js
@@ -7,17 +7,44 @@ var bodyParser = require("body-parser");
 var PORT = 3000;
 var path = require("path") 
 
+var table1 = [];
+var table2 = [];
+var table3 = [];
+var table4 = [];
+var table5 = [];
+var reservations = [];
+
+
+/* USER ROUTES */
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
+    res.sendFile(path.join(__dirname, "home.html"));
 });
 
 app.get('/tables', function (req, res) {
-  res.sendFile(path.join(__dirname, "tables.html"));
+    res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 app.get('/reserve', function (req, res) {
-  res.sendFile(path.join(__dirname, "reserve.html"));
+    res.sendFile(path.join(__dirname, "reserve.html"));
 });
+
+app.get("/api/reservations", function (req, res) {
+    return res.json(reservations)
+})
+
+app.get("/api/characters/:reservation", function (req, res) {
+    for (i = 0; i < reservations.length; i++) {
+      return res.json(reservations[i]);
+    }
+})
+
+app.post("/api/characters", function (req, res) {
+  var newReservation = req.body;
+
+  reservations.push(newReservation);
+
+  res.json(newReservation)
+})
  
 
 
@@ -33,9 +60,5 @@ app.listen(PORT, function() {
 })
 
 
-var table1 = [];
-var table2 = [];
-var table3 = [];
-var table4 = [];
-var table5 = [];
+
 
